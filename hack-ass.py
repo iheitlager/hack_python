620b5f8e86668ac623f7eb79194289216e863075
Ilja Heitlager <ilja@heitlager.com>
2023-12-25 12:23:51 +0100

    ruff format test

--- hack-ass.py
+++ hack-ass.py
@@ -3,75 +3,96 @@ import argparse
 
 from hack_python import assemble
 
-CEND      = '\33[0m'
+CEND = "\33[0m"
 
-CBLACK  = '\33[30m'
-CRED    = '\33[31m'
-CGREEN  = '\33[32m'
-CYELLOW = '\33[33m'
-CBLUE   = '\33[34m'
+CBLACK = "\33[30m"
+CRED = "\33[31m"
+CGREEN = "\33[32m"
+CYELLOW = "\33[33m"
+CBLUE = "\33[34m"
+
 
 def dump_lines(code_table, out, binary=False, verbose=False):
     for c in code_table:
         if c.code is not None:
+            s = "{address:04x}: "
             if binary:
                 code = str(bin(c.code))[2:].zfill(16)
-                s = "{address:04x}: " + code.ljust(17) + " {raw}\n"
+                s += code.ljust(17) + " {raw}\n"
             else:
-                code = str(hex(c.code))[2:].zfill(4).upper()
+                code = str(hex(c.code))[2:].zfill(4).upper().ljust(11)
                 if verbose and c.a_value is not None:
-                    s = "{address:04x}:       " + CYELLOW + code.ljust(11) + CEND + CBLUE + " {raw}" + CEND + "\n"
+                    s += "      " + CYELLOW + code + CEND + CBLUE + " {raw}" + CEND + "\n"
                 elif verbose:
-                    s = "{address:04x}:       " + CYELLOW + code.ljust(11) + CEND + " {raw}\n"
+                    s += "      " + CYELLOW + code + CEND + " {raw}\n"
                 else:
-                    s = "{address:04x}:       " + code.ljust(11) + " {raw}\n"
+                    s += code + " {raw}\n"
         elif c.label:
             if verbose:
-                s = "{address:04x}: " + CBLUE + "({label})" + CEND + "\n"
+                s += CBLUE + "({label})" + CEND + "\n"
             else:
-                s = "{address:04x}: ({label})\n"
+                s += "({label})\n"
         else:
-            s = "{address:04x}: " + "".ljust(17) + " {raw}\n"
+            s += "".ljust(17) + " {raw}\n"
         out.write(s.format(**c.__dict__))
+
 
 def dump_bin(code_table, out, binary=True):
     for c in code_table:
         if c.code is not None:
             if binary:
-                code = str(bin(c.code))[2:].zfill(16)+'\n'
+                code = str(bin(c.code))[2:].zfill(16) + "\n"
             else:
-                code = str(hex(c.code))[2:].zfill(4).upper()+'\n'
+                code = str(hex(c.code))[2:].zfill(4).upper() + "\n"
             out.write(code)
+
 
 def dump_symbols(symbol_table, out, verbose=False):
     out.write("\n\nSymbols:\n")
-    for symbol, value in sorted(symbol_table.items(), key=lambda x:0 if x[1] is None else x[1]):
+    for symbol, value in sorted(
+        symbol_table.items(), key=lambda x: 0 if x[1] is None else x[1]
+    ):
         if verbose:
-            out.write(("{0:10}: " + CYELLOW + "{1:04x}" + CEND + "\n").format(symbol, value or 0))
+            out.write(
+                ("{0:10}: " + CYELLOW + "{1:04x}" + CEND + "\n").format(
+                    symbol, value or 0
+                )
+            )
         else:
             out.write("{0:10}: {1:04x}\n".format(symbol, value or 0))
 
 
-
-
 def main():
-    parser = argparse.ArgumentParser(
-        description="Assemble files for Hack"
+    parser = argparse.ArgumentParser(description="Assemble files for Hack")
+    parser.add_argument(
+        "--version", action="version", version=f"{parser.prog} version 1.0.0"
     )
-    parser.add_argument("--version", action="version",
-        version = f"{parser.prog} version 1.0.0")
-    parser.add_argument("-v", "--verbose", action="store_true",
-        help = "be verbose in assembling")
-    parser.add_argument("-d", "--dump", action="store_true",
-        help="dump in human readable format")
-    parser.add_argument("--debug", action="store_true",
-        help="dump in human readable format")
-    parser.add_argument("-b", "--binary", action="store_true",
-        help="use binary in dump")
-    parser.add_argument('file', nargs='?', type=argparse.FileType('r'),
-        default=sys.stdin, help="file to assemble or use stdin")
-    parser.add_argument("-o", "--outfile", type=argparse.FileType('w'),
-        default=sys.stdout, help="file to output or use stdout")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="be verbose in assembling"
+    )
+    parser.add_argument(
+        "-d", "--dump", action="store_true", help="dump in human readable format"
+    )
+    parser.add_argument(
+        "--debug", action="store_true", help="dump in human readable format"
+    )
+    parser.add_argument(
+        "-b", "--binary", action="store_true", help="use binary in dump"
+    )
+    parser.add_argument(
+        "file",
+        nargs="?",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="file to assemble or use stdin",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="file to output or use stdout",
+    )
     args = parser.parse_args()
 
     asm = assemble.assembler(debug=args.debug)
@@ -80,12 +101,16 @@ def main():
     asm.pass_two(lines)
 
     if args.dump:
-        dump_lines(asm.code_table, out=args.outfile, verbose=args.verbose, binary=args.binary)
+        dump_lines(
+            asm.code_table, out=args.outfile, verbose=args.verbose, binary=args.binary
+        )
         dump_symbols(asm.symbol_table, verbose=args.verbose, out=args.outfile)
     else:
         dump_bin(asm.code_table, out=args.outfile, binary=args.binary)
 
-    if args.file != sys.stdin: args.file.close()
+    if args.file != sys.stdin:
+        args.file.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
