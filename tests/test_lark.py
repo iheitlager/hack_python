67314c7b95127e1fc099623373a7a64f57f98e60
Ilja Heitlager <ilja@heitlager.com>
2024-01-19 07:11:04 +0100

    working first lark on hello world

--- tests/test_lark.py
+++ tests/test_lark.py
@@ -1,19 +1,17 @@
 import lark
-from pathlib import Path
 
-grammar = '''class Main {
+
+base_grammar = '''class Main { 
     function void main() {
-
- //       do Output.printString("Hello World");
- //       do Output.println();
-	
-    	return;
+        do Output.printString("Hello World");
+        do Output.println();
+        return;
     }
-
 }'''
 
-def test_grammar():
+def test_base_grammar():
     lark_path = '../src/hack_python/jack/jack.lark'
     parser = lark.Lark.open(lark_path, rel_to=__file__, parser="lalr")
-    tree = parser.parse(grammar)
-    print("All grammars parsed successfully")
+    tree = parser.parse(base_grammar)
+    print(tree)
+    assert isinstance(tree, lark.Tree)
